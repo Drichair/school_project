ae09158a9eaec7dbbe98d29630dbecccf764a82d
Danila Kalinin <danila.vovf@yandex.ru>
2022-02-06 16:48:34 +0300

    1st

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def get_base_context(request, pagename):
+    return {
+        'pagename': pagename,
+    }
+
+
+def index_page(request):
+    context = get_base_context(request, 'PythonBin')
+    return render(request, 'main/index.html', context)
+
